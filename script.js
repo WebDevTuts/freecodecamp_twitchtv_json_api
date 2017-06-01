698f6fa3ce08d9a0fde49eecd41d5da4eb6dc073
Edwin Chen <edwinchen85@gmail.com>
2017-06-01 13:35:54 +0800

    Reference freecodecamp twitch.tv url. Get JSON API with given url.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // Run our jQuery
 $(document).ready(function() {
+  // Free Code Camp stream info and status API call
+  var url = "https://api.twitch.tv/kraken/streams/freecodecamp";
+  $.getJSON(url, function(data1) {
 
+  });
 });
