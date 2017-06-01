f4fb2c48ecb95852e0cabe7ca9b9e0f30ab71c77
Edwin Chen <edwinchen85@gmail.com>
2017-06-01 13:59:07 +0800

    Get JSON data from freecodecamp twitch.tv account. Check if freecodecamp is offline or online.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Run our jQuery
 $(document).ready(function() {
   // Free Code Camp stream info and status API call
-  var url = "https://api.twitch.tv/kraken/streams/freecodecamp";
+  var url = "https://api.twitch.tv/kraken/channels/freecodecamp/?client_id=qzsmdq24e2j4upt48jj6jm68u00n2r4";
   $.getJSON(url, function(data1) {
-
+    if (data1.stream === null) {
+      $('#fccStatus').html('Free Code Camp is currently OFFLINE!');
+    } else {
+      $('#fccStatus').html('Free Code Camp is currently ONLINE!');
+    }
   });
 });
