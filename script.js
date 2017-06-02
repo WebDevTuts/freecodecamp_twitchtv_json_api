0f2a996671601a38714e2c703d831befcce55f63
Edwin Chen <edwinchen85@gmail.com>
2017-06-02 21:13:29 +0800

    Reference fccURI and fetch it

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ $(document).ready(function() {
 
 function fetchAll(status) {
   $("#twitch-list").empty();
+
+  var fccURI = "https://api.twitch.tv/kraken/channels/freecodecamp/?client_id=qzsmdq24e2j4upt48jj6jm68u00n2r4";
+  fetch(fccURI, true, status);
 }
