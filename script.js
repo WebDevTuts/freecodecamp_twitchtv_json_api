64c8ab079c49a7db9b6a0433e8d809eeda83c8bf
Edwin Chen <edwinchen85@gmail.com>
2017-06-02 21:16:23 +0800

    Define fetch func

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ function fetchAll(status) {
   var featuredURI = "https://api.twitch.tv/kraken/streams/featured?client_id=qzsmdq24e2j4upt48jj6jm68u00n2r4";
   fetch(featuredURI, false, status);
 }
+
+function fetch(apiURL, includeFCC, status) {
+
+}
