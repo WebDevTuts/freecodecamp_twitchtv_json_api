6408a89998360fc02faa11e5a556bfb2925b69a5
Edwin Chen <edwinchen85@gmail.com>
2017-06-02 21:19:25 +0800

    Define ajax success method

--- script.js
+++ script.js
@@ -15,6 +15,55 @@ function fetchAll(status) {
 function fetch(apiURI, includeFCC, status) {
   $.ajax({
     type: 'GET',
-    url: apiURI
+    url: apiURI,
+    success: function(data) {
+      var json = "";
+      var twitchItem = "";
+      var twitchURI = "";
+      var twitchTitle = "";
+      var twitchText = "";
+      var twitchLogo = "";
+      var twitchStreaming = "offline";
+
+      if (includeFCC == false) {
+        json = data.featured;
+
+        json.forEach(function(val) {
+          twitchTitle = val.title;
+          twitchText = val.text;
+
+          var fixedText = twitchText.substring(0, twitchText.indexOf("<a"));
+
+          if (fixedText.length < 100) {
+            return;
+          }
+          twitchURI = val.stream.channel.url;
+          twitchLogo = val.stream.channel.logo;
+
+          if (val.stream != '') {
+            twitchStreaming = "Online";
+          } else {
+            twitchStreaming = "Offline";
+          }
+
+          twitchItem = "<a href=\"" + twitchURI + "\" class=\"list-group-item\" > <span class=\"badge badger\">" + twitchStreaming + "</span><img class=\"img-logo img-circle\" src=\"" + twitchLogo + "\"></img><h3 class=\"list-group-item-heading\">" + twitchTitle + "</h3>" + "<p class=\"list-group-item-text\">" + fixedText +  "</p></a>";
+
+          if (status === "all") {
+            $("#twitch-list").append(twitchItem);
+          } else if (status === "online" && twitchStreaming == "Online") {
+            $("#twitch-list").append(twitchItem);
+          } else if (status === "offline" && twitchStreaming == "Offline") {
+            $("#twitch-list").append(twitchItem);
+          }
+        });
+      } else {
+        twitchTitle = data.name;
+        twitchText = data.status  + "<br/><br/><br/><br/><br/>";
+        twitchURI = data.url;
+        twitchLogo = data.log;
+        twitchStreaming = "Offline";
+
+        twitchItem = "<a href=\"" + twitchURI + "\" class=\"list-group-item\" > <span class=\"badge badger\">" + twitchStreaming + "</span><img class=\"img-logo img-circle\" src=\"" + twitchLogo + "\"></img><h3 class=\"list-group-item-heading\">" + twitchTitle + "</h3>" + "<p class=\"list-group-item-text\">" + twitchText +  "</p></a>";
+    }
   })
 }
