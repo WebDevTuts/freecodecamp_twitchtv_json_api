bab0b00842751332f1d73eee7be0a6ad81acbd28
Edwin Chen <edwinchen85@gmail.com>
2017-06-02 21:15:52 +0800

    Reference featuredURI and fetch it

--- script.js
+++ script.js
@@ -7,4 +7,7 @@ function fetchAll(status) {
 
   var fccURI = "https://api.twitch.tv/kraken/channels/freecodecamp/?client_id=qzsmdq24e2j4upt48jj6jm68u00n2r4";
   fetch(fccURI, true, status);
+
+  var featuredURI = "https://api.twitch.tv/kraken/streams/featured?client_id=qzsmdq24e2j4upt48jj6jm68u00n2r4";
+  fetch(featuredURI, false, status);
 }
